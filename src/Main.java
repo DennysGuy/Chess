d9dd8267b57e9f36c9a385e3d47abf0dff2ac976
DennysGuy <mattmatriciano@gmail.com>
2022-12-27 13:40:56 -0800

    I'm Committing

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,18 @@ import pieces.*;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.printBoard();
+        int choice = -1;
+        Scanner kb = new Scanner(System.in);
+        do{
+            menu();
+            choice = kb.nextInt();
 
+        }while(choice < 0);
 
+        if (choice == 1) {
+            clearConsole();
+            Board board = new Board();
+            board.printBoard();
+        }
     }
 }
